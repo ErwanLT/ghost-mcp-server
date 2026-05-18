dac74b54e043b38e8a5a080b435cfe0e166ead4f
Erwan LE TUTOUR <erwanletutour.elt@gmail.com>
2026-05-18 06:02:38 +0200

    init

---
+++ src/main/java/fr/eletutour/ghostmcpserver/GhostMcpServerApplication.java
@@ -0,0 +1,13 @@
+package fr.eletutour.ghostmcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhostMcpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/eletutour/ghostmcpserver/GhostMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eletutour.ghostmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
